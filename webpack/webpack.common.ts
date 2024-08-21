30c7a4981052c1c6dec23f085480f991816f792d
HinyariGohan <hinyarigohan@gmail.com>
2024-08-22 01:41:42 +0900

    chore: Update TypeScript and Webpack configuration

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -1,14 +1,8 @@
-import path from 'path';
 import nodeExternals from 'webpack-node-externals';
 import { DefinePlugin, type Configuration } from 'webpack';
 
 const config: Configuration = {
-  target: 'node', // Node.js 環境用に設定
-  entry: './src/app.ts', // エントリポイント
-  output: {
-    filename: 'app.js', // 出力ファイル名
-    path: path.resolve(__dirname, './'), // 出力ディレクトリ
-  },
+  target: 'node',
   module: {
     rules: [
       {
@@ -19,9 +13,9 @@ const config: Configuration = {
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'], // 解決可能な拡張子
+    extensions: ['.ts', '.js'],
   },
-  externals: [nodeExternals()], // Node.js の組み込みモジュールをバンドルから除外
+  externals: [nodeExternals()],
   plugins: [
     new DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,27 +1,10 @@
-import path from 'path';
-import nodeExternals from 'webpack-node-externals';
+import { merge } from 'webpack-merge';
 import type { Configuration } from 'webpack';
+import common from './webpack.common';
 
-const config: Configuration = {
-  target: 'node', // Node.js 環境用に設定
-  entry: './src/app.ts', // エントリポイント
-  output: {
-    filename: 'app.js', // 出力ファイル名
-    path: path.resolve(__dirname, './'), // 出力ディレクトリ
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'], // 解決可能な拡張子
-  },
-  externals: [nodeExternals()], // Node.js の組み込みモジュールをバンドルから除外
+const devConfig: Configuration = {
+  mode: 'development',
+  devtool: 'inline-source-map',
 };
 
-export default config;
+export default merge(common, devConfig);
