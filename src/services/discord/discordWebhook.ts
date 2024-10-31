f5af3030a20f69f0798e242eaa9bb39cbadfdb25
HinyariGohan <hinyarigohan@gmail.com>
2024-10-31 10:49:50 +0900

    Discord Webhookメッセージ送信関数の引数を変更し、エラーログ出力を改善しました。無限ループを防ぐためにloggerからconsole.errorに変更しました。

--- src/services/discord/discordWebhook.ts
+++ src/services/discord/discordWebhook.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { logger } from '../../utils/logger';
 
-export async function sendDiscordWebhookMessage(message: string, webhook: string): Promise<void> {
+export async function sendDiscordWebhookMessage(
+  webhookUrl: string,
+  message: string
+): Promise<void> {
   const payload = {
     content: message,
   };
 
-  await axios.post(webhook, payload).catch((err) => {
-    logger.error('Error in sendMessageToDiscordChannel: ' + err);
+  await axios.post(webhookUrl, payload).catch((err) => {
+    // logger.error をすると無限ループになる可能性があるので console.error でエラーを出力
+    console.error('Error in sendMessageToDiscordChannel: ' + err);
   });
 }
