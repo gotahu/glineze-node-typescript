4f8c456a3c1e98141602cd9743d371ba571fa4e7
HinyariGohan <hinyarigohan@gmail.com>
2024-11-02 22:59:44 +0900

    新しいコマンド「施錠状態」を追加し、施錠デバイスの状態を取得して返信する機能を実装しました。エラーハンドリングも追加しました。

---
+++ src/services/discord/commands/sesame.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js';
+import { SesameService } from '../../sesame/sesameService';
+import { format } from 'date-fns';
+import { StatusMessage } from '../../../types/types';
+import { logger } from '../../../utils/logger';
+
+export async function handleSesameStatusCommand(sesameService: SesameService, message: Message) {
+  try {
+    const status = await this.sesameService.getSesameDeviceStatus();
+    const dateStr = format(new Date(status.timestamp), 'yyyy-MM-dd HH:mm:ss');
+    message.reply(`施錠状態: ${StatusMessage[status.lockStatus]}, タイムスタンプ：${dateStr}`);
+  } catch (error) {
+    logger.error('施錠状態を取得できませんでした ' + error);
+    message.reply('施錠状態を取得できませんでした ' + error);
+  }
+}
