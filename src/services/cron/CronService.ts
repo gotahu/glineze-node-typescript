3ca39097294948cff58d73ab05806c990da50624
HinyariGohan <hinyarigohan@gmail.com>
2025-01-01 02:00:42 +0900

    feat: Add countdown scheduler to CronService start method

--- src/services/cron/CronService.ts
+++ src/services/cron/CronService.ts
@@ -23,7 +23,7 @@ export class CronService {
   public start() {
     // ここで複数のジョブをスケジュール登録したりする
     this.startSesameScheduler();
-    // ほかのジョブがあればそれらもここで start
+    this.startCountdownScheduler();
   }
 
   /**
