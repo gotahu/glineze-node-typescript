b4556873ea3420aeef7389e2014aaea4a54579c5
HinyariGohan <hinyarigohan@gmail.com>
2025-01-01 15:32:17 +0900

    feat: Refactor countdown date calculation and add calculateDiffDate function

--- src/services/discord/countdown.ts
+++ src/services/discord/countdown.ts
@@ -16,20 +16,8 @@ async function updateChannelTopic(discord: DiscordService) {
   const targetDateString = config.getConfig('date_of_countdown_date');
   const targetEventName = config.getConfig('name_of_countdown');
 
-  // 日付文字列の妥当性をチェック
-  if (!isValidDateString(targetDateString)) {
-    console.error('無効な日付文字列です:', targetDateString);
-    return;
-  }
-
-  // 対象日を日本時間の00:00:00に設定
-  const TARGET_DATE = startOfDay(parseISO(targetDateString));
-
-  // 今日の日付を日本時間の00:00:00に設定
-  const today = startOfDay(new Date());
-
-  // 今日からターゲット日付までの日数を計算
-  const daysLeft = differenceInDays(TARGET_DATE, today);
+  // 今日からの日数を計算
+  const daysLeft = calculateDiffDate(targetDateString);
 
   // チャンネルを取得
   const channel = discord.client.channels.cache.get(CHANNEL_ID) as TextChannel;
@@ -56,14 +44,8 @@ function updateBotProfile(discord: DiscordService) {
   const targetDateString = config.getConfig('date_of_countdown_date');
   const targetEventName = config.getConfig('name_of_countdown');
 
-  // 対象日を日本時間の00:00:00に設定
-  const TARGET_DATE = startOfDay(parseISO(targetDateString));
-
-  // 今日の日付を日本時間の00:00:00に設定
-  const today = startOfDay(new Date());
-
-  // 今日からターゲット日付までの日数を計算
-  const daysLeft = differenceInDays(TARGET_DATE, today);
+  // 今日からの日数を計算
+  const daysLeft = calculateDiffDate(targetDateString);
 
   // 日数に応じてメッセージを変更
   let topicMessage: string;
@@ -79,6 +61,28 @@ function updateBotProfile(discord: DiscordService) {
   discord.client.user.setActivity(topicMessage, { type: ActivityType.Custom });
 
   logger.info(`ボットのステータスを更新しました: ${topicMessage}`, { debug: true });
+}
+
+/**
+ * 日付文字列を受け取り、今日からの日数を計算する関数
+ * @param targetDateString ISO形式の日付文字列
+ * @returns 今日からの日数
+ */
+function calculateDiffDate(targetDateString: string) {
+  // 日付文字列の妥当性をチェック
+  if (!isValidDateString(targetDateString)) {
+    throw new Error('無効な日付文字列です');
+    return;
+  }
+
+  // 対象日を日本時間の00:00:00に設定
+  const TARGET_DATE = startOfDay(parseISO(targetDateString));
+
+  // 今日の日付を日本時間の00:00:00に設定
+  const today = startOfDay(new Date());
+
+  // 今日からターゲット日付までの日数を計算
+  return differenceInDays(TARGET_DATE, today);
 }
 
 export { updateChannelTopic, updateBotProfile };
